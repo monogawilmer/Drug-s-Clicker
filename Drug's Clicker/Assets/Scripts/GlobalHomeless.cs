7c1c7a29363368c8d2eaa178e935b5809143e4b0
monogawilmer <43412081+monogawilmer@users.noreply.github.com>
2019-06-20 09:37:12 -0500

    Update

---
+++ Drug's Clicker/Assets/Scripts/GlobalHomeless.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalHomeless : MonoBehaviour
+{
+    public GameObject fakeButton;
+    public GameObject fakeText;
+    public GameObject realButton;
+    public GameObject realText;
+    public int currentCash;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        currentCash = GlobalCash.cashAmount;
+    }
+}
