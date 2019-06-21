ba247dd9075acd490a620a8ba771f054a7627b5b
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2019-06-21 17:41:20 -0500

    first city 75% complete

--- Drug's Clicker/Assets/Scripts/BoughtCities.cs
+++ Drug's Clicker/Assets/Scripts/BoughtCities.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoughtCities : MonoBehaviour
 {
     public int priceCity;
+
+    public GameObject nextCity;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,14 @@ public class BoughtCities : MonoBehaviour
     void Update()
     {
         
+        
     }
     public void ClickTheButton()
     {
         if (GlobalCash.cashAmount>=priceCity)
         {
             GlobalCash.cashAmount =GlobalCash.cashAmount - priceCity;
+            nextCity.SetActive(true);
         }
     }
 }
