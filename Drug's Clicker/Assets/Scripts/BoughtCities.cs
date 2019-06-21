dfea3c49307fd8cd83facf6bf822aca3e50978cf
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2019-06-21 16:55:59 -0500

    first city

---
+++ Drug's Clicker/Assets/Scripts/BoughtCities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoughtCities : MonoBehaviour
+{
+    public int priceCity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ClickTheButton()
+    {
+        if (GlobalCash.cashAmount>=priceCity)
+        {
+            GlobalCash.cashAmount =GlobalCash.cashAmount - priceCity;
+        }
+    }
+}
