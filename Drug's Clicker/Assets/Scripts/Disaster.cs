941b275e07261e1cc5b55d6ede15941ffae9ebfc
monogawilmer <43412081+monogawilmer@users.noreply.github.com>
2019-06-24 13:14:47 -0500

    Perdidas Ocasionales

---
+++ Drug's Clicker/Assets/Scripts/Disaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Disaster : MonoBehaviour
+{
+    public GameObject statusBox;
+    public float drugCheck;
+    public int genChance;
+    public bool disasterActive = false;
+    public int drugLoss;
+
+    void Update()
+    {
+        drugCheck = GlobalDrug.drugAmount / 10;
+        if(disasterActive == false)
+        {
+            StartCoroutine(StarDisaster());
+        }
+    }
+
+    IEnumerator StarDisaster()
+    {
+        disasterActive = true;
+        genChance = Random.Range(1, 20);
+        if(drugCheck >= genChance)
+        {
+            drugLoss = Mathf.RoundToInt(GlobalDrug.drugAmount * 0.25f);
+            statusBox.GetComponent<Text>().text = "You have lost " + drugLoss + " because a laboratory have burned";
+            GlobalDrug.drugAmount -= drugLoss;
+            //yield return new WaitForSeconds(3);
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+            yield return new WaitForSeconds(1);
+            statusBox.SetActive(false);
+            statusBox.SetActive(true);
+        }
+        yield return new WaitForSeconds(10);
+        disasterActive = false;
+    }
+}
