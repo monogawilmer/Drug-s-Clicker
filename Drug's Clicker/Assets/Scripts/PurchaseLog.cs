f4e6d02fae54f99ca4fc696a7f4b304891799688
monogawilmer <43412081+monogawilmer@users.noreply.github.com>
2019-06-24 20:00:40 -0500

    Drug'sClicker v1

--- Drug's Clicker/Assets/Scripts/PurchaseLog.cs
+++ Drug's Clicker/Assets/Scripts/PurchaseLog.cs
@@ -13,7 +13,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalHomeless.homelessValue;
         GlobalHomeless.homelessValue *= 2;
         GlobalHomeless.turnOffButton = true;
-        GlobalHomeless.homelessPerSec += 10;
+        GlobalHomeless.homelessPerSec += 10*2;
         GlobalHomeless.numberOfHomeless += 1;  
     }
 
@@ -23,7 +23,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalStudent.studentValue;
         GlobalStudent.studentValue *= 2;
         GlobalStudent.turnOffButton = true;
-        GlobalStudent.studentPerSec += 50;
+        GlobalStudent.studentPerSec += 50*2;
         GlobalStudent.numberOfStudent += 1;
     }
 
@@ -33,7 +33,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalDealer.dealerValue;
         GlobalDealer.dealerValue *= 3;
         GlobalDealer.turnOffButton = true;
-        GlobalDealer.dealerPerSec += 100;
+        GlobalDealer.dealerPerSec += 100*2;
         GlobalDealer.numberOfDealer += 1;
     }
 
@@ -43,7 +43,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalHitman.hitmanValue;
         GlobalHitman.hitmanValue *= 3;
         GlobalHitman.turnOffButton = true;
-        GlobalHitman.hitmanPerSec += 500;
+        GlobalHitman.hitmanPerSec += 500*2;
         GlobalHitman.numberOfHitman += 1;
     }
 
@@ -53,7 +53,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalFarmer.farmerValue;
         GlobalFarmer.farmerValue *= 4;
         GlobalFarmer.turnOffButton = true;
-        GlobalFarmer.farmerPerSec += 1000;
+        GlobalFarmer.farmerPerSec += 1000*2;
         GlobalFarmer.numberOfFarmer += 1;
     }
 
@@ -63,7 +63,7 @@ public class PurchaseLog : MonoBehaviour
         GlobalDrug.drugAmount -= GlobalPolitician.politicianValue;
         GlobalPolitician.politicianValue *= 4;
         GlobalPolitician.turnOffButton = true;
-        GlobalPolitician.politicianPerSec += 5000;
+        GlobalPolitician.politicianPerSec += 5000*2;
         GlobalPolitician.numberOfPolitician += 1;
     }
 }
